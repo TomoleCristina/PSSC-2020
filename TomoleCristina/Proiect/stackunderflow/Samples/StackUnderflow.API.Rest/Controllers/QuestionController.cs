07183a12e25f5f14966a62ac0fb21bb456d900dc
Tomole Cristina <tomole_cristina@yahoo.com>
2020-11-23 22:33:37 +0200

    Start Project

---
+++ TomoleCristina/Proiect/stackunderflow/Samples/StackUnderflow.API.Rest/Controllers/QuestionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Access.Primitives.Extensions.ObjectExtensions;
+using Access.Primitives.IO;
+using Microsoft.AspNetCore.Mvc;
+using StackUnderflow.Domain.Core;
+using StackUnderflow.Domain.Core.Contexts;
+using StackUnderflow.Domain.Schema.Backoffice.CreateTenantOp;
+using StackUnderflow.EF.Models;
+using Access.Primitives.EFCore;
+using StackUnderflow.Domain.Schema.Backoffice.InviteTenantAdminOp;
+using StackUnderflow.Domain.Schema.Backoffice;
+using LanguageExt;
+using StackUnderflow.Domain.Core.Contexts.Question.CreateQuestion;
+using StackUnderflow.Domain.Core.Contexts.Question;
+
+namespace StackUnderflow.API.AspNetCore.Controllers
+{
+        [ApiController]
+        [Route("question")]
+        public class QuestionController : ControllerBase
+        {
+            private readonly IInterpreterAsync _interpreter;
+            private readonly StackUnderflowContext _dbContext;
+
+            public QuestionController(IInterpreterAsync interpreter, StackUnderflowContext dbContext)
+            {
+                _interpreter = interpreter;
+                _dbContext = dbContext;
+            }
+
+            [HttpPost("CreateQuestion")]
+            public async Task<IActionResult> CreateQuestion(CreateQuestionCmd cmd)
+            {
+                var dep = new QuestionDependencies();
+                var ctx = new QuestionWriteContext();
+
+                var expr = from createTenantResult in QuestionContext.CreateQuestion(cmd)
+                           select createTenantResult;
+
+                var r = await _interpreter.Interpret(expr, ctx, dep);
+
+                return r.Match(
+                    created => Ok(created),
+                    notCreated => BadRequest(notCreated),
+                    invalidRequest => ValidationProblem()
+                    );
+            }
+        }
+    }
---
+++ TomoleCristina/Proiect/stackunderflow/Samples/StackUnderflow.Core/Contexts/Question/CreateQuestion/CreateQuestionCmd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestion
+{
+    /// <summary>
+    /// SUM type Title * Body * Tags
+    /// </summary>
+    public class CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }           //titlul intrebarii
+        [Required]
+        public string Body { get; private set; }            //descrierea detaliala a intrebarii
+        [Required]
+        public string Tags { get; set; }                     //domeniul/domeniile din care face parte intrebarea
+        public CreateQuestionCmd(string title, string body, string tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+    }
+}
